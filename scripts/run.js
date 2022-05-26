e0053f3d77ca39e504656b8abb8f24a94fd9ebad
Young <phanton0117@gmail.com>
2022-05-27 01:17:01 +0800

    add: compile contract locally and run it

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
+  //Compile contract and generate the necessary files we need to work with contract under artifacts dir
+  //hre (Hardhat Run Environment)
+  //command :"npx hardhat ..." to get hre object built on the fly using the hardhat.config.js
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+
+  //Hardhat will create a local Ethereum network for us (only for this contract)
+  //Then, after the script completes it'll destroy that local network
   const waveContract = await waveContractFactory.deploy();
+
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
 };
