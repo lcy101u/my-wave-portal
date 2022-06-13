6223bf7ccf18c5cf747b96e95cc5ab77114a9c40
Young <phanton0117@gmail.com>
2022-06-13 18:30:06 +0800

    feat: Randomly pick a winner and prevent spammers

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -17,25 +18,47 @@ contract WavePortal {
 
     Wave[] waves;
 
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
-        console.log("Yo, I'm a contract and I'm running on the blockchain ...");
+        console.log("We have been constructed!");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-min bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Must wait 30 seconds for another waving :)"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves++;
         console.log("%s has waved, message - %s", msg.sender, _message);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        // Generate a new seed for the next user that sends a wave
+        seed = (block.timestamp + block.difficulty) % 100;
+        console.log("Random # generated: %d", seed);
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+            uint256 prizeAmount = 0.0001 ether; //solidity lets us use the ether unit
+            require(
+                prizeAmount <= address(this).balance, //`address(this).balance` is the balance of the contract itself
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
 
-        uint256 prizeAmount = 0.0001 ether; //solidity lets us use the ether unit
-        require(
-            prizeAmount <= address(this).balance, //`address(this).balance` is the balance of the contract itself
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
--- scripts/run.js
+++ scripts/run.js
@@ -23,15 +23,13 @@ const main = async () => {
     waveContract.address
   );
 
-  // let waveCount;
-  // waveCount = await waveContract.getTotalWaves();
-  // console.log('waveCount', waveCount.toNumber());
-
   /* 
    * Send Wave 
    */
-  let waveTxn = await waveContract.wave('A message!');
+  const waveTxn = await waveContract.wave('This is wave #1');
   await waveTxn.wait();
+  const waveTxn2 = await waveContract.wave('This is wave #2');
+  await waveTxn2.wait();
 
   /*
    * Get Contract balance to see what happened
